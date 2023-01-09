0821bfbd49244e8d5a66bd7b8e8de63cbe5bdeda
Logan Hampton <losesmc@gmail.com>
2023-01-09 09:14:45 -0700

    Fux config Proxy to get non path items & format

--- src/ai_config.js
+++ src/ai_config.js
@@ -1,29 +1,35 @@
-
-import { workspace, Uri, FileType} from 'vscode';
-import { showInformationMessage, showErrorMessage } from './ai_showMessage';
+import { workspace, Uri, FileType } from 'vscode';
+import { showErrorMessage } from './ai_showMessage';
 import { findFilepath } from './util';
+
 const conf = {
   data: workspace.getConfiguration('autoit'),
-  defaultPaths:{
-    aiPath: {file: "AutoIt3.exe"},
-    wrapperPath: {dir: "SciTE\\AutoIt3Wrapper\\", file: "AutoIt3Wrapper.au3"},
-    checkPath: {file: "AU3Check.exe"},
-    helpPath: {file: "AutoIt3Help.exe"},
-    infoPath: {file: "Au3Info.exe"},
-    kodaPath: {dir: "SciTE\\Koda\\", file: "FD.exe"},
-    includePaths: [{dir: ""}],
-    smartHelp: {check: {dir: "Advanced.Help\\HelpFiles\\", file: ""}}
-  }
+  defaultPaths: {
+    aiPath: { file: 'AutoIt3.exe' },
+    wrapperPath: { dir: 'SciTE\\AutoIt3Wrapper\\', file: 'AutoIt3Wrapper.au3' },
+    checkPath: { file: 'AU3Check.exe' },
+    helpPath: { file: 'AutoIt3Help.exe' },
+    infoPath: { file: 'Au3Info.exe' },
+    kodaPath: { dir: 'SciTE\\Koda\\', file: 'FD.exe' },
+    includePaths: [{ dir: '' }],
+    smartHelp: { check: { dir: 'Advanced.Help\\HelpFiles\\', file: '' } },
+  },
 };
 
 const listeners = new Map();
-let listenerId = 0, aiPath = "", _noEvents;
+let listenerId = 0;
+let aiPath = '';
+let _noEvents;
 workspace.onDidChangeConfiguration(({ affectsConfiguration }) => {
   if (_noEvents || !affectsConfiguration('autoit')) return;
 
   conf.data = workspace.getConfiguration('autoit');
   listeners.forEach(listener => {
-    try{listener();}catch(er){console.error(er);}
+    try {
+      listener();
+    } catch (er) {
+      console.error(er);
+    }
   });
   getPaths();
 });
@@ -50,7 +56,7 @@ function splitPath(path) {
   };
 }
 
-function fixPath (value, data) {
+function fixPath(value, data) {
   const path = splitPath(value || "");
   const file = data.file;
   const dir = data.dir;
@@ -59,14 +65,14 @@ function fixPath (value, data) {
 
   if (path.dir === "" || path.isRelative)
     path.dir = aiPath.dir + path.dir + (!path.isRelative ? (dir || "") : "");
-  
+
   if (file === undefined)
     path.file += "/";
 
   return (path.dir + "/" + path.file).replace(/[\\/]+/g, "\\");
 }
 
-function verifyPath (path, data, msgSuffix) {
+function verifyPath(path, data, msgSuffix) {
 
   return workspace.fs.stat(Uri.parse(`file:///${data.fullPath}`)).then(stats => {
     const type = (data.file !== undefined ? FileType.File : FileType.Directory) | FileType.SymbolicLink;
@@ -85,8 +91,7 @@ function verifyPath (path, data, msgSuffix) {
   });
 }
 
-function showError(path, data, msgSuffix)
-{
+function showError(path, data, msgSuffix) {
   if (!msgSuffix)
     return;
 
@@ -103,8 +108,7 @@ function showError(path, data, msgSuffix)
   data.prevCheck = path;
 }
 
-function updateFullPath(path, data, msgSuffix)
-{
+function updateFullPath(path, data, msgSuffix) {
   if (path !== "")
     data.fullPath = fixPath(path, data);
 
@@ -115,9 +119,9 @@ function updateFullPath(path, data, msgSuffix)
 }
 
 function getPaths() {
-  aiPath = splitPath(conf.data.aiPath||"");
+  aiPath = splitPath(conf.data.aiPath || "");
 
-  for(let i in conf.defaultPaths) {
+  for (let i in conf.defaultPaths) {
     const defaultPath = conf.defaultPaths[i],
       confValue = conf.data[i];
 
@@ -133,14 +137,14 @@ function getPaths() {
           continue;
 
         const chmPath = val.chmPath.trim(),
-          data = Object.assign({fullPath: ""}, defaultPath.check),
+          data = Object.assign({ fullPath: "" }, defaultPath.check),
           udfPath = Array.isArray(val.udfPath) ? [...val.udfPath] : val.udfPath.split("|"),
           msgSuffix = `${i}.${prefix}`;
 
         updateFullPath(chmPath, data, `${msgSuffix}.chmPath`);
 
-        for(let k = 0; k < udfPath.length; k++){
-          const data = Object.assign({fullPath: ""}, defaultPath.check);
+        for (let k = 0; k < udfPath.length; k++) {
+          const data = Object.assign({ fullPath: "" }, defaultPath.check);
           updateFullPath(udfPath[k], data).then(filePath => {
             if (!filePath && !(filePath = findFilepath(udfPath[k], true))) {
               showError(udfPath[k], data, `${msgSuffix}.udfPath[${k}]`);
@@ -156,12 +160,12 @@ function getPaths() {
       }
     }
     else if (Array.isArray(confValue)) {
-      for (let j = 0; j < confValue.length; j++ ) {
+      for (let j = 0; j < confValue.length; j++) {
         const path = (typeof confValue[j] == 'string' ? confValue[j] : '').trim();
 
         if (defaultPath[j] === undefined)
-          defaultPath[j] = Object.assign({fullPath: ""}, defaultPath[0].check);
-  
+          defaultPath[j] = Object.assign({ fullPath: "" }, defaultPath[0].check);
+
         updateFullPath(path, defaultPath[j], `${i}[${j}]`);
       }
     }
@@ -187,28 +191,26 @@ function upgradeSmartHelpConfig() {
     };
 
   let ret = {}, ConfigurationTarget, overrideInLanguage;
-  for(let i in props) {
+  for (let i in props) {
     if (inspect[i] !== undefined) {
       [ConfigurationTarget, overrideInLanguage] = props[i];
       break;
     }
   }
   if (Array.isArray(data)) {
-    for(let i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
       ret[data[i][0]] = {
         chmPath: data[i][1],
         udfPath: data[i][2].split("|")
       };
     }
   }
-  if (!Object.keys(ret).length || typeof data == "string")
-    ret = undefined;
+  if (!Object.keys(ret).length || typeof data === 'string') ret = undefined;
 
-  conf.data.update("smartHelp", ret, ConfigurationTarget, overrideInLanguage);
+  conf.data.update('smartHelp', ret, ConfigurationTarget, overrideInLanguage);
 }
 
-function noEvents(value)
-{
+function noEvents(value) {
   _noEvents = value;
 }
 
@@ -217,19 +219,21 @@ const config = new Proxy(conf, {
     const val = target.defaultPaths[prop];
     if (val) {
       const isArray = Array.isArray(val);
-      if (isArray || (val !== null && typeof val == "object"))
+      if (isArray || (val !== null && typeof val === 'object'))
         return isArray ? val.map(a => a.fullPath) : val.fullPath;
 
       return val.fullPath;
     }
-    return val;
+    return target.data[prop];
   },
-  set(target, prop, val) { return target.data.update(prop, val); }
+  set(target, prop, val) {
+    return target.data.update(prop, val);
+  },
 });
 
 export default {
   config,
   addListener,
   removeListener,
-  noEvents
+  noEvents,
 };
