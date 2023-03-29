be5c32223375d41dacd94088dc086b4fbed535e8
Logan Hampton <losesmc@gmail.com>
2023-03-29 15:45:17 -0700

    Fixes for AutoItDefinitionProvider
    Addresses (part of) #153. Should ignore "definitions" in comments.

--- src/ai_definition.js
+++ src/ai_definition.js
@@ -2,44 +2,83 @@ import { languages, Location, Position, Uri } from 'vscode';
 import { AUTOIT_MODE, getIncludePath, getIncludeText, getIncludeScripts } from './util';
 
 const AutoItDefinitionProvider = {
+  /**
+   * Finds the definition of a word in a document and returns its location.
+   * @param {TextDocument} document - The document in which to search for the word definition.
+   * @param {Position} position - The position of the word for which to find the definition.
+   * @returns {Location|null} - The location of the word definition, or null if not found.
+   */
   provideDefinition(document, position) {
     const lookupRange = document.getWordRangeAtPosition(position);
-    const lookup = document.getText(lookupRange);
-    const docText = document.getText();
-    let defRegex;
+    const lookupText = document.getText(lookupRange);
+    const documentText = document.getText();
 
-    if (lookup.charAt(0) === '$') {
-      defRegex = new RegExp(`((?:Local|Global|Const)\\s*)?\\${lookup}\\s+?=?`, 'i');
-    } else {
-      defRegex = new RegExp(`(Func\\s+)${lookup}\\s*\\(`);
-    }
+    const definitionRegex = this.determineRegex(lookupText);
+    let match = documentText.match(definitionRegex);
 
-    let found = docText.match(defRegex);
-
-    if (found) {
-      return new Location(document.uri, document.positionAt(found.index + (found[1] || '').length));
+    if (match) {
+      return new Location(document.uri, document.positionAt(match.index + (match[1] || '').length));
     }
 
     // If nothing was found, search include files
+    match = this.findDefinitionInIncludeFiles(documentText, definitionRegex, document);
+
+    if (match) {
+      const { scriptPath } = match;
+      const scriptContentBeforeMatch = match.scriptContent
+        .slice(0, match.found.index + (match[1] || '').length)
+        .split('\n');
+      const matchLine = scriptContentBeforeMatch.length - 1;
+      const matchCharacterIndex =
+        scriptContentBeforeMatch[scriptContentBeforeMatch.length - 1].length;
+      return new Location(Uri.file(scriptPath), new Position(matchLine, matchCharacterIndex));
+    }
+
+    return null;
+  },
+
+  /**
+   * Determines the regex for a given lookup string.
+   * @param {string} lookup - The lookup string.
+   * @returns {RegExp} The regex for the lookup string.
+   */
+  determineRegex(lookup) {
+    if (lookup.charAt(0) === '$') {
+      return new RegExp(
+        `(?<![;].*)(?<!(?:#cs|#comments-start).*)((?:Local|Global|Const)\\s*)?\\${lookup}\\s+?=.*(?![^#]*(#ce|#comments-end))`,
+        'i',
+      );
+    }
+    return new RegExp(
+      `(?<![;].*)(?<!(?:#cs|#comments-start).*)(Func\\s+)${lookup}\\s*\\((?![^#]*(#ce|#comments-end))`,
+    );
+  },
+
+  /**
+   * Searches the included scripts in a document for a definition matching a regular expression.
+   * @param {string} docText - The text of the document.
+   * @param {RegExp} defRegex - The regular expression to search for.
+   * @param {TextDocument} document - The document being searched.
+   * @returns {object|null} - An object containing information about the found definition, or null if not found.
+   */
+  findDefinitionInIncludeFiles(docText, defRegex, document) {
     const scriptsToSearch = [];
     getIncludeScripts(document, docText, scriptsToSearch);
 
-    if (scriptsToSearch.length) {
-      found = null;
-      for (let i = 0; i < scriptsToSearch.length; i += 1) {
-        const scriptPath = getIncludePath(scriptsToSearch[i], document);
-        const scriptContent = getIncludeText(scriptPath);
+    const searchScript = (script, returnObject = false) => {
+      const scriptPath = getIncludePath(script, document);
+      const scriptContent = getIncludeText(scriptPath);
+      const found = scriptContent.match(defRegex);
 
-        found = scriptContent.match(defRegex);
+      if (returnObject) return { scriptPath, scriptContent, found };
 
-        if (found) {
-          const arr = scriptContent.slice(0, found.index + (found[1] || '').length).split('\n');
-          const line = arr.length - 1;
-          const char = arr[arr.length - 1].length;
+      return found;
+    };
 
-          return new Location(Uri.file(scriptPath), new Position(line, char));
-        }
-      }
+    const match = scriptsToSearch.find(searchScript);
+
+    if (match) {
+      return searchScript(match, true);
     }
 
     return null;
